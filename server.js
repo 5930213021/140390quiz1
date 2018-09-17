2eff50b8a13212c4978ec872c4ff5f5d4b665bc9
nisarat <nisaratnsaelim@gmail.com>
2018-09-17 16:30:41 +0700

    edit

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var express = require('express');
-
 var app = express();
-
-
 
 
 app.set('view engine', 'ejs');
@@ -15,3 +12,5 @@ app.get('/home',function(req,res){
     res.render('pages/home');
 });
 
+console.log('App is running at http://localhost:8080');
+app.listen(8080); //8080 ไว้สำหรับtest app
