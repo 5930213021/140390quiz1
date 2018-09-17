abb534ab13c9a0e19d1d5214a0868ad4efcb92ea
nisarat <nisaratnsaelim@gmail.com>
2018-09-17 17:37:22 +0700

    edit server

--- server.js
+++ server.js
@@ -20,26 +20,27 @@ var connection = mysql.createConnection({
   database : 'db140390'
 });
 
+connection.connect()
+
 //Display all Students
 app.get('/students',function(req,res){
-connection.connect()
 connection.query('select * from students', function (err, rows, fields) {
   if (err) throw err
     res.render('pages/students',{students : rows})
   console.log('The solution is: ', rows[0].solution)
 })
-connection.end()
 });
 
 //Display all Subjects
 app.get('/subjects',function(req,res){
-connection.connect()
+//connection.connect()
 connection.query('select * from subjects', function (err, rows, fields) {
   if (err) throw err
     res.render('pages/subjects',{subjects : rows})
   console.log('The solution is: ', rows[0].solution)
+  //connection.end()
 })
-connection.end()
+
 });
 
 console.log('App is running at http://localhost:8081');
